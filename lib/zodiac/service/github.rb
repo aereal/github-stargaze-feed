2f3a5e6351548e29ed03beb31a306cdd328244af
aereal <aereal@aereal.org>
2016-02-15 02:37:35 +0900

    + GitHub custom client

---
+++ lib/zodiac/service/github.rb
@@ -0,0 +1,24 @@
+require 'faraday/rack_builder'
+require 'octokit'
+require 'yaml'
+
+require 'zodiac/middleware/cache'
+
+module Zodiac
+  module Service
+    module GitHub
+      def self.new(host, with_cache: false, logging: false)
+        hub_config = YAML.load_file(File.expand_path('~/.config/hub'))
+        github_config = hub_config.fetch(host, []).first or abort 'No configuration found'
+        Octokit::Client.new(access_token: github_config.fetch('oauth_token')).tap do |this|
+          stack = Faraday::RackBuilder.new do |builder|
+            builder.use Zodiac::Middleware::Cache if with_cache
+            builder.response :logger if logging
+            builder.adapter Faraday.default_adapter
+          end
+          this.middleware = stack
+        end
+      end
+    end
+  end
+end
